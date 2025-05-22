ac4cb5d3e0af91f9e163b3e1b5f3cb75b5784ba9
Zalzameen21 <zalzameen2002@gmail.com>
2025-05-22 14:24:36 +0530

    Create Q16.sh

---
+++ COMPUTER NETWORK/Q16.sh
@@ -0,0 +1,28 @@
+Q)Shell script to count the number of words and lines in a file
+
+
+Code:
+if [ $# -gt 1 ]
+then
+        echo "syntax is <$0> [<filename>]"
+        exit 1
+fi
+flag=0
+if [ $# -eq 1 ]
+then
+        term=`tty`
+        exec <$1
+fi
+while read line
+do
+        no1=` expr $no1 + 1 `
+        set $line>/dev/null
+        nowds=` expr $nowds + $# `
+done
+echo "numbrer of lines=$no1"
+echo "number of words=$nowds"
+if [ $flag -eq 1 ]
+then
+        exec < $term
+fi
+exit 0
