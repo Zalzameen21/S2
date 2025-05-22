65bb28e8009a3d88281f68cf006e77d96b157e50
Zalzameen21 <zalzameen2002@gmail.com>
2025-05-22 14:46:50 +0530

    Q20.sh

--- COMPUTER NETWORK/Q20.sh
+++ COMPUTER NETWORK/Q20.sh
@@ -21,30 +21,3 @@ head -n $n2 "$1" | tail -n $n3
 shift
 done
 exit 0
-
-Input:
---------------------file1.txt--------------------------
-Hello good morning!
-This is the Second line
-This is the Third line
-
--------------------------------------------------------
-sh range.sh file1.txt 2-3
-
-Output:
-lines from 2 to 3 from Q8.sh
-
-This is the Second line
-This is the Third line
-
-
-Algorithm:
-Step 1: If args ≤ 2, show syntax and exit.
-Step 2: Set n1 = start line, n2 = end line.
-Step 3: Remove first two args (lines), keep files.
-Step 4: Find number of lines to print: n3 = n2 - n1 + 1.
-Step 5: For each file:
- – Print header with line range and filename.
- – Print lines from n1 to n2 using head and tail.
-Step 6: Repeat for all files.
-Step 7: Exit.
