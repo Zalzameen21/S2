308289da52b36cac9c616815c64cbf405e14cb4e
Zalzameen21 <zalzameen2002@gmail.com>
2025-05-22 14:46:33 +0530

    Q19.sh

--- COMPUTER NETWORK/Q19.sh
+++ COMPUTER NETWORK/Q19.sh
@@ -15,24 +15,3 @@ f_size=`cat $f | wc -c`
 echo -e "$f \t\t $f_size \t $fdatetime"
 fi
 done
-
-
-Algorithm:
-Step 1: Show current directory path.
-Step 2: For each file/dir in current folder.
-Step 3: Get its last modified date & time.
-Step 4: If it's a directory, print name, <DIR>, and time.
-Step 5: If it's a file, get size in bytes and print name, size, and time.
-Step 6: Repeat till all items are done.
-Step 7: End.
-
-
-Input:
-sh window.sh file1.txt
-
-Output:
- Directory of /c/Users/MCA19/Desktop/New folder
-
-A.SH.1610                225     22-05-25 13:35:05
-b.sh                     265     22-05-25 14:09:00
-
