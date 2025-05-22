35e075632bcf14880767dd6148e64f999754cd89
Zalzameen21 <zalzameen2002@gmail.com>
2025-05-22 14:44:31 +0530

    Q14.sh

--- COMPUTER NETWORK/Q14.sh
+++ COMPUTER NETWORK/Q14.sh
@@ -19,24 +19,3 @@ then
         echo "file renamed to .$$ "
         fi
 done
-Input:
-sh rename.sh
-ls
-
-Output:
-file renamed to .790
-
-
-
-
-
-#rename.sh.790*
-
-Algorithm:
-Step 1: Check if the number of arguments is greater than 1.
-        If true, print a syntax error message and exit.
-Step 2: If exactly 1 argument is provided, change the current directory to the specified location.
-Step 3: Use a for loop to go through each item in the current directory.
-Step 4: For each item, check if it is a regular file.
-        If true, rename the file by appending the current process ID ($$) to its name.
-Step 5: After renaming, print a message showing the new file name.
