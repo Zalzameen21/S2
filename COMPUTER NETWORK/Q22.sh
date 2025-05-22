90492dedcedbcf8e55d974770cbc6c6b53f78868
Zalzameen21 <zalzameen2002@gmail.com>
2025-05-22 14:48:47 +0530

    Q22.sh

--- COMPUTER NETWORK/Q22.sh
+++ COMPUTER NETWORK/Q22.sh
@@ -23,31 +23,3 @@ done
 shift
 done
 echo "no.of occurance of words = $count"
-
-Algorithm:
-Step 1: If no args given, show syntax and exit.
-Step 2: Set word to first arg, then remove it from args.
-Step 3: Set count to 0.
-Step 4: For each file in args:
- – Read each word in the file.
- – If word matches word, increase count by 1.
-Step 5: Move to next file until done.
-Step 6: Print total count of occurrences.
-Step 7: End.
-
-
-Input:
-
-vi file1.txt
-
- ----------------------file1.txt--------------------
- Hello good morning!
- This is the first line
- This is the second line
--------------------------------------------------------
-
-sh wordoccurance.sh This file1.txt 
-
-
-Output:
-no.of occurance of words = 2
