2c6ab37f2f52c87d974550682871277a9b9862f7
Zalzameen21 <zalzameen2002@gmail.com>
2025-05-22 14:47:13 +0530

    Q21.sh

--- COMPUTER NETWORK/Q21.sh
+++ COMPUTER NETWORK/Q21.sh
@@ -17,34 +17,3 @@ shift
 shift
 done
 exit 0
-
-
-Algorithm:
-Step 1: Check if number of args is even; if not, show error and exit.
-Step 2: Loop through args two at a time.
-Step 3: Copy first file to second file.
-Step 4: Print copy confirmation.
-Step 5: Shift by two to next pair.
-Step 6: Repeat until no args left.
-Step 7: Exit.
-
-Input:
----------------------file1.txt--------------------------
-Hello good morning!
-This is the first line
-This is the second line
--------------------------------------------------------
-
-$ sh copy.sh file.txt newfile.txt
-
-Output:
-Copied 'file.txt' to 'newfile.txt'
-
-
-
-vi newfile.txt
----------------------newfile.txt--------------------------
-Hello good morning!
-This is the first line
-This is the second line
--------------------------------------------------------
