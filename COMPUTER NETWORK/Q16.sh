a0752d5bdb2d2a1f511f50f67545a56793b3e083
Zalzameen21 <zalzameen2002@gmail.com>
2025-05-22 14:45:07 +0530

    Update Q16.sh

--- COMPUTER NETWORK/Q16.sh
+++ COMPUTER NETWORK/Q16.sh
@@ -28,23 +28,3 @@ fi
 exit 0
 
 
-Algorithm:
-Step 1: If more than one argument, show syntax and exit.
-Step 2: Set line and word counters to 0.
-Step 3: If filename given, read input from it.
-Step 4: For each line:
- – Increase line count.
- – Count words and add to total.
-Step 5: Print line and word counts.
-Step 6: Restore terminal input if changed.
-Step 7: Exit program.
-
-
-Input:
-$ sh wordcount.sh file1.txt
-
-
-Output:
-numbrer of lines=2
-number of words=4
-
